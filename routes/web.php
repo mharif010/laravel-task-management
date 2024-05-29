8171c3fdc7f905a9a6164f7f644aadfca47c105a
mharif <mh.arif.web@gmail.com>
2024-05-29 17:09:52 +0600

    added after projects view and tasks view template

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/tasks', function () {
+    return view('tasks');
+})->name('tasks');
+
+Route::get('/projects', function () {
+    return view('projects');
+})->name('projects');
